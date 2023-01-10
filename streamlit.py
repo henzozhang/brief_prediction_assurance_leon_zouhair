4e834c7a255d69cff22ccc61eec97920aebf4d5c
leonzhang <zhangleon1985@gmail.com>
2023-01-10 14:15:51 +0100

    modeliation termine

--- streamlit.py
+++ streamlit.py
@@ -3,6 +3,16 @@ import pandas as pd
 import numpy as np
 import pickle
 import sklearn as sk
+# from modelisation import categorie_bmi
+def categorie_bmi(bmi):
+    if bmi < 18.5:
+        return 'faible'
+    elif bmi < 25:
+        return 'normal'
+    elif bmi < 30:
+        return 'surpoids'
+    else:
+        return 'obesité'
 
 
 
@@ -23,6 +33,8 @@ height = st.number_input("la taille(metre)",value=1.75)
 
 bmi = weight/(height ** 2)
 
+categorie_bmi = categorie_bmi(bmi)
+
 sex = st.radio("le sexe  ", ('female', 'male'), ) 
 
 smoker = st.radio("fumeur ou non-fumeur ", ('yes', 'no'), )
@@ -35,8 +47,8 @@ pickle_in = open('my_pipe_ridge.pkl', 'rb')
 my_pipe_ridge = pickle.load(pickle_in) 
 
 
-donnee =np.array([[age, sex, bmi, children, smoker, region]])
-df_X=pd.DataFrame(donnee,columns=['age', 'sex', 'bmi', 'children', 'smoker', 'region'])
+donnee =np.array([[age, sex, categorie_bmi, children, smoker, region]])
+df_X=pd.DataFrame(donnee,columns=['age', 'sex', 'categorie_bmi', 'children', 'smoker', 'region'])
 
 if(st.button('Calculate')): 
     prediction = my_pipe_ridge.predict(df_X)
